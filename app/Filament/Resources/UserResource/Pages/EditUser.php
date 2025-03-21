3c7f752319ea232bffc3b02552afa658d9c8875b
johhann <johannesm1219@gmail.com>
2025-03-22 02:33:24 +0300

    fix: update users error

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role;
 
 class EditUser extends EditRecord
 {
@@ -15,5 +17,20 @@ class EditUser extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        $record->update([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        // remove user roles
+        $role = Role::findById($data['role_id']);
+        $record->syncRoles([$role]);
+
+        return $record;
     }
 }
